3cef2986dbac7058cc593a33cb7534dff0df4287
Josephinemwende <josymwesh45@gmail.com>
2023-02-18 04:16:39 -0800

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
@@ -13,5 +12,6 @@ int main(void)
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
 		putchar('\n');
+
 	return (0);
 }
