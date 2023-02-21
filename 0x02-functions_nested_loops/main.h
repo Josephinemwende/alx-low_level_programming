cdbe70478c03669e59f80406dd565a8bcf75633b
Josephinemwende <josymwesh45@gmail.com>
2023-02-21 01:34:38 -0800

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,1 @@
-#include <stdio.h>
-#include "main.h"
 
-/**
- * main - Entry point
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	putchar(_putchar);
-	putchar('\n');
-
-	return (0);
-}
