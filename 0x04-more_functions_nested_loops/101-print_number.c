969985229a867680522444dc40680cf53530c14a
Josephinemwende <josymwesh45@gmail.com>
2023-02-23 15:31:42 -0800

    prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+void print_integer(int x);
+
+/**
+ * print_number - prints an integer
+ * @n: input integer
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+	if (n == 0)
+		_putchar('0');
+	else if  (n < 0)
+	{
+		_putchar('-');
+		print_integer(n * -1);
+	}
+	else
+		print_integer(n);
+}
+
+/**
+ * print_integer - prints n
+ * @x: input
+ * Return: Nothing
+ */
+
+void print_integer(int x)
+{
+	int a = 1000;
+
+	for (; a >= 1; a /= 10)
+		if (x / a != 0)
+		{
+			_putchar((x / a) % 10 + '0');
+		}
+}
