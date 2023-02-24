35ecd51c5e18356d2b88e3bb8a5e005682fb94ae
Josephinemwende <josymwesh45@gmail.com>
2023-02-23 17:07:40 -0800

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x, sum = 0;
+
+	for (x = 0; x < 1024; x++)
+	{
+		if ((x % 5) == 0 ||  (x  % 3) == 0)
+			sum += x;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
