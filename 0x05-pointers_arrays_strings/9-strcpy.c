17cfb7758f9be0d944780c7f382febad0bcd5fcf
Josephinemwende <josymwesh45@gmail.com>
2023-02-27 12:56:18 -0800

    copy the string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,9 +12,20 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	char *x = dest;
+	int l, i;
 
-	while (*src)
-		*dest++ = *src++;
-	return (x);
+	l = 0;
+
+	while (src[l] != '\0')
+	{
+		l++;
+	}
+
+	for (i = 0; i < l; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
